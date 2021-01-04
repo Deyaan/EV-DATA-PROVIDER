a1747fd8a1de8cf9b7d2d1a0b72adcb7a786de0f
Deyaan <65954790+Deyaan@users.noreply.github.com>
2021-01-04 16:53:30 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,43 @@
+  const fname=document.getElementById('name');
+  const email=document.getElementById('email');
+  const data=document.getElementById('data');
+  const id=document.getElementById('id');
+  const machine=document.getElementById('machine');
+  const number=document.getElementById('number');
+
+  const submitbtn=document.getElementById('submitbtn');
+  const editbtn=document.getElementById('editbtn');
+
+  var database;
+
+  //Submit data
+  submitbtn.addEventListener('click', (e) =>{
+    database = firebase.database();
+    e.preventDefault();
+    alert('submitted!');
+
+    database.ref('/Admin/' + id.value).set({
+      Name : fname.value,
+      Email : email.value,
+      Number : number.value,
+      Data : data.value,
+      Machine :machine.value
+    });
+  });
+  
+  //Update Data
+  editbtn.addEventListener('click', (e) =>{
+    database = firebase.database();
+    const rootRef = database.ref('Admin');
+    e.preventDefault();
+    alert('updated!!!');
+
+    const newData = {
+      Name : fname.value,
+      Email : email.value,
+      Number : number.value,
+      Data : data.value,
+      Machine : machine.value
+    }
+    rootRef.child(id.value).update(newData);
+  });
